4bd354156f37ee50b41c84635500df9004d4b863
Fernando Oliveira <devphytols@gmail.com>
2024-03-19 17:11:37 -0300

    Update Presence Pic

--- src/app/presence/page.tsx
+++ src/app/presence/page.tsx
@@ -27,7 +27,7 @@ export default function PresencePage() {
             <body className="bg-white min-height-full" style={{ backgroundColor: "#000000!important" }}>
                 <div className="d-md-flex min-height-full">
                     <div className="flex-self-stretch bg-gray-dark col-md-5 col-lg-4 col-xl-3 px-4 px-md-6 px-lg-7 py-6" style={{ borderRight: '1px solid #181818' }}>
-                        <img src="https://i.imgur.com/0XSmsJ9.png" className="circle mb-3" style={{ maxWidth: '250px' }} title="" />
+                        <img src="https://i.imgur.com/2HtWKIa.png" className="circle mb-3" style={{ maxWidth: '250px' }} title="" />
                         <h1 className="text-white mb-2 lh-condensed">Fernando Oliveira</h1>
                         <p className="mb-3 f4 text-white">
                             A self-taught full-stack developer
